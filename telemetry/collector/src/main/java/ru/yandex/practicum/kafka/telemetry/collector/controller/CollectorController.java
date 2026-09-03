9109efecd8926ef72aa512d5cd06cc7849a48087
Anna <mussina1anna@yandex.ru>
2026-09-03 08:28:21 +0300

    Исправлен импорт AvroSerializer, добавлен AppConfig, удалён старый сериализатор

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/controller/CollectorController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/controller/CollectorController.java
@@ -1,9 +1,8 @@
 package ru.yandex.practicum.kafka.telemetry.collector.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,48 +10,55 @@ import ru.yandex.practicum.kafka.telemetry.collector.dto.HubEvent;
 import ru.yandex.practicum.kafka.telemetry.collector.dto.SensorEvent;
 import ru.yandex.practicum.kafka.telemetry.collector.mapper.HubEventMapper;
 import ru.yandex.practicum.kafka.telemetry.collector.mapper.SensorEventMapper;
-import ru.yandex.practicum.kafka.telemetry.collector.util.AvroSerializer;
+import ru.yandex.practicum.kafka.telemetry.collector.service.KafkaEventProducer;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.util.AvroSerializer;
 
+@Slf4j
 @RestController
 public class CollectorController {
 
-    private static final Logger log = LoggerFactory.getLogger(CollectorController.class);
-    private static final String HUBS_TOPIC = "telemetry.hubs.v1";
-    private static final String SENSORS_TOPIC = "telemetry.sensors.v1";
-
-    private final KafkaTemplate<String, byte[]> kafkaTemplate;
     private final SensorEventMapper sensorEventMapper;
     private final HubEventMapper hubEventMapper;
     private final AvroSerializer avroSerializer;
+    private final KafkaEventProducer producer;
 
-    public CollectorController(KafkaTemplate<String, byte[]> kafkaTemplate,
-                               SensorEventMapper sensorEventMapper,
+    @Autowired
+    public CollectorController(SensorEventMapper sensorEventMapper,
                                HubEventMapper hubEventMapper,
-                               AvroSerializer avroSerializer) {
-        this.kafkaTemplate = kafkaTemplate;
+                               AvroSerializer avroSerializer,
+                               KafkaEventProducer producer) {
         this.sensorEventMapper = sensorEventMapper;
         this.hubEventMapper = hubEventMapper;
         this.avroSerializer = avroSerializer;
+        this.producer = producer;
     }
 
     @PostMapping("/events/sensors")
     public ResponseEntity<Void> handleSensorEvent(@RequestBody SensorEvent event) {
-        log.debug("Received sensor event: {}", event);
+        log.info("Received sensor event: {}", event);
+
         SensorEventAvro avroEvent = sensorEventMapper.toAvro(event);
+
         byte[] avroBytes = avroSerializer.serialize(avroEvent);
-        kafkaTemplate.send(SENSORS_TOPIC, event.getHubId(), avroBytes);
+
+        producer.sendSensorEvent(avroEvent, event.getHubId(), avroBytes);
+
         log.info("Sensor event sent to Kafka: id={}, hubId={}", event.getId(), event.getHubId());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/events/hubs")
     public ResponseEntity<Void> handleHubEvent(@RequestBody HubEvent event) {
-        log.debug("Received hub event: {}", event);
+        log.info("Received hub event: {}", event);
+
         HubEventAvro avroEvent = hubEventMapper.toAvro(event);
+
         byte[] avroBytes = avroSerializer.serialize(avroEvent);
-        kafkaTemplate.send(HUBS_TOPIC, event.getHubId(), avroBytes);
+
+        producer.sendHubEvent(avroEvent, event.getHubId(), avroBytes);
+
         log.info("Hub event sent to Kafka: hubId={}", event.getHubId());
         return ResponseEntity.ok().build();
     }
