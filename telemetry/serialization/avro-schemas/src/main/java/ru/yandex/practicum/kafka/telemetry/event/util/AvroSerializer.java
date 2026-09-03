41c59def7b37a6d163dfe648f820436f16ebe3f2
Anna <mussina1anna@yandex.ru>
2026-09-03 08:14:50 +0300

    Убрана Spring-зависимость из AvroSerializer, создан бин в AppConfig

--- telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/telemetry/event/util/AvroSerializer.java
+++ telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/telemetry/event/util/AvroSerializer.java
@@ -1,16 +1,14 @@
-package ru.yandex.practicum.kafka.telemetry.collector.util;
+package ru.yandex.practicum.kafka.telemetry.event.util;
 
 import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DatumWriter;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.avro.specific.SpecificRecordBase;
-import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-@Component
 public class AvroSerializer {
 
     public byte[] serialize(SpecificRecordBase record) {
